96407a739cc1037fd38ce938025703751ac4ec66
piyushsarkale23 <piyushsarkale35@gmail.com>
2023-04-05 18:44:42 +0530

    Add project files.

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(83, 136);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(74, 26);
+            this.txtname.TabIndex = 0;
+            this.txtname.Text = "car name";
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(83, 69);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(74, 26);
+            this.txtid.TabIndex = 1;
+            this.txtid.Text = "car id";
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(180, 136);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 26);
+            this.txt2.TabIndex = 2;
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(180, 69);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 26);
+            this.txt1.TabIndex = 3;
+            this.txt1.TextChanged += new System.EventHandler(this.txt1_TextChanged);
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(132, 261);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(75, 69);
+            this.btnsave.TabIndex = 4;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.txtname);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Button btnsave;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("idiot");
+        }
+
+        private void txt1_TextChanged(object sender, EventArgs e)
+        {
+            if(txt1.Text =="1")
+            {
+                MessageBox.Show("good");
+            }
+            if (txt2.Text == "ooo")
+            {
+                MessageBox.Show("goodnnn");
+            }
+        }
+    }
+}
